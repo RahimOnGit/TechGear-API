413c6327dcda593075a90d2bb09b4e4f9cfa0544
RahimOnGit <abdalraheem.saif7777@gmail.com>
2025-02-21 12:36:37 +0100

    part2 GET customer , PUT customer added

--- customer.js
+++ customer.js
@@ -1,14 +1,40 @@
 const db = require('./db');
 
-function getCustomerById()
-{
 
+
+
+
+function editCustomer(req, res, name, email, phone) {
+    const sql = `
+      UPDATE CUSTOMER
+      SET NAME = ?, EMAIL = ?, PHONENUMBER = ?
+      WHERE ID = ?;
+    `;
+  
+    db.run(sql, [name, email, phone, req.params.id], function (err) {
+      if (err) {
+        console.log("Error in updating customer data:", err.message);
+        return res.status(500).json({ message: "Internal server error" });
+      }
+      res.status(200).json({ message: "Updated successfully", name, email, phone });
+    });
+  }
+
+function getCustomerById(req,res,id)
+{
+const sql = `select * from customer 
+join orders on orders.CUSTOMER_ID = customer.ID
+where customer.ID = ?;  `;
+db.all(sql,[id],(err,rows)=>{
+
+if(!rows || rows.length==0)
+{
+   return res.status(404).json({ message: "customer not found"});
+}
+res.status(200).json(rows);
+})
 }
 
-function editCustomer()
-{
-
-}
 
 function getCustomerOrder()
 {
--- server.js
+++ server.js
@@ -1,38 +1,37 @@
 
 const express = require('express');
 const app = express();
-const db = require('./product');
+const p = require('./product');
+const c = require('./customer');
+
+const db = require('./db');
 
 
 app.use(express.json())
-
+/* products */
+//get all product
 app.get('/products', (req, res) => {
-    db.getAllProducts(res); 
+    p.getAllProducts(res); 
 });
 
-
+//search by category
 app.get('/products/category/:id', (req, res) => {
-    db.getProductByCategory(res,req.params.id); 
+    p.getProductByCategory(res,req.params.id); 
 });
 
-
+//search by name
 app.get('/products/search', (req, res) => {
     const productName = req.query.name;
     if (!productName) {
         return res.status(400).json({ message: "Product name is required" });
     }
-    db.getProductbyName(res, productName);
+    p.getProductbyName(res, productName);
 });
-
-
-
-
-
+//search by id
 app.get('/products/:id', (req, res) => {
-    db.getProductById(res,req.params.id); 
+    p.getProductById(res,req.params.id); 
 });
-
-
+//post
 app.post("/products",(req,res)=>{
 
 const {name , price , stock , category_id, manifacture_id ,review_id} = req.body;
@@ -43,28 +42,49 @@ const {name , price , stock , category_id, manifacture_id ,review_id} = req.body
 db.addProduct(req,res,name , price , stock , category_id, manifacture_id ,review_id);
 
 })
-
+//delete by id
 app.delete("/products/:id",(req,res)=>{
     const {id} = req.params;
    
-    db.deleteProduct(req,res,id);
+    p.deleteProduct(req,res,id);
 
 });
 
-   
+//edit   
 app.put("/products/:id",(req,res)=>{
-  
-    
 const {name , price , stock , category_id, manifacture_id ,review_id} = req.body;
 if(!name || !price ||!stock || !category_id|| !manifacture_id)
 {
   return res.status(500).json({message: "you missed required entry"});
 }
-
-    db.editProduct(req,res,name,price,stock,category_id,manifacture_id,review_id);
+    p.editProduct(req,res,name,price,stock,category_id,manifacture_id,review_id);
 })
 
 
+
+//customer 
+app.get('/customer/', (req, res) => {
+    db.all(`select * from customer`,(err,rows)=>
+    {
+        res.json(rows);
+    }
+    );
+});
+
+
+app.get('/customer/:id', (req, res) => {
+    c.getCustomerById(req,res,req.params.id); 
+});
+
+app.put('/customer/:id',(req,res)=>{
+    const {name , email , phone} = req.body;
+   
+if(!name||!email||!phone)
+{
+    return res.status(400).json({message:" u missed a required entity"})
+}
+    c.editCustomer(req,res,name,email,phone);
+})
 
 app.listen(7000, () => {
     console.log('Server started on port 7000');
